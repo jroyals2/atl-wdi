aa9f387e07cf150dfec8afbefe1cf8caa76c7f6a
jroyals2 <jamesroyals3@gmail.com>
2017-09-06 16:36:10 -0400

    classwork lab work answers

--- labs/javaScript/js-functions/functions-lab-starter.js
+++ labs/javaScript/js-functions/functions-lab-starter.js
@@ -1,42 +1,71 @@
 // Question 1
-function maxOfTwoNumbers() {
-
+function maxOfTwoNumbers(x, y) {
+    if (x > y){
+        return x;
+    }
+    return y;
 }
 
 // Question 2
-function maxOfThree() {
-
+function maxOfThree(x, y, z) {
+    if (x > y && x > z){
+        return x;
+    } else if (y > x && y > z) {
+        return y;
+    }
+    return z;
 }
 
 // Question 3
-function isCharacterAVowel() {
-
-}
+function isCharacterAVowel(c) {
+    var vowels = ['a','e','i','o','u','A','E','I','O','U' ];
+    if (vowels.indexOf(c) !== -1) {
+      return true;
+    } 
+    return false;
+  }
 
 // Question 4
 function sumArray() {
- 
+    function sumArray(arr) {
+        var sum = 0;
+        for (var i = 0; i < arr.length; i++) {
+          sum = arr[i] + sum;
+        }
+        return sum;
+      }
 }
 
 
 // Question 4
 function multiplyArray() {
+    function sumArray() {
+        function sumArray(arr) {
+            var product = 1;
+            for (var i = 0; i < arr.length; i++) {
+              product = arr[i] * product;
+            }
+            return product;
+          }
+    }
 
 }
 
 
 // Question 5
 var numberOfArguments = function(){
-  
+  return argue.length;
 }
 
 
 
 // Question 6
-var reverseString = function (){
-  
-};
-
+var reverseString = function (str){
+    const splitString = str.split("");
+    const reverseSplit = splitString.reverse();
+    const reverseString = reverseSplit.join("");
+    return reverseString;
+  };
 
 // Question 7
 function findLongestWord () {
