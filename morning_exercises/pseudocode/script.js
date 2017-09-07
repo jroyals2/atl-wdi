024439658d4e538d4f476e9cada23cf7aaa0f8c1
jroyals2 <jamesroyals3@gmail.com>
2017-09-07 09:29:06 -0400

    Morning exercise done

--- morning_exercises/pseudocode/script.js
+++ morning_exercises/pseudocode/script.js
@@ -3,6 +3,9 @@
 // Barrels O' RUM
 
 var barrels = function(small, large, total){
+  totalLarge = total - (small * 60);
+  costOfLarge = totalLarge / large;
+  return costOfLarge;
 };
 
 //*************************
@@ -10,7 +13,10 @@ var barrels = function(small, large, total){
 // Sailing the Seas
 
 var shipFuelCost = function(fuelPrice, milesPerGallon){
-  //your code here
+  const earth = 24901;
+  totalGallons = earth/milesPerGallon;
+  totalPrice = totalGallons * fuelPrice;
+  return totalPrice;
 };
 
 //*************************
@@ -18,7 +24,12 @@ var shipFuelCost = function(fuelPrice, milesPerGallon){
 // GROG
 
 var calcFruitJuice = function(a, b, c, d){
-  //your code here
+const totalFruitCostco= ((b / 100) * a);
+const totalFruitKirk = (d / 100) * c;
+const totalPure = totalFruitCostco + totalFruitKirk;
+const totalGall = (a + c);
+const everything = totalPure / totalGall;
+return everything;
 };
 
 //DO NOT EDIT BELOW THIS LINE//
