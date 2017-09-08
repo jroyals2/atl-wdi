5e5f47afba8952686e0d85c45ec0108e69204407
jroyals2 <jamesroyals3@gmail.com>
2017-09-08 10:32:00 -0400

    morning exercise stuff

--- morning_exercises/javaScript/making-change/index.js
+++ morning_exercises/javaScript/making-change/index.js
@@ -5,7 +5,18 @@
 /// DO NOT EDIT ABOVE THIS LINE ///
 
 var makeChange = function(price, payment) {
-  // Your Code Here
+  let leftOver = Math.round(100*payment - 100*price);
+if (leftOver < 0){return [0,0,0,0]}
+else {
+const quarters = Math.floor(leftOver / 25);
+leftOver = leftOver % 25;
+const dimes = Math.floor(leftOver / 10);
+leftOver = leftOver % 10;
+const nickles = Math.floor(leftOver / 5);
+leftOver = leftOver % 5;
+const pennies = Math.floor(leftOver);
+return [quarters, dimes, nickles, pennies];
+}
 };
 
 /// DO NOT EDIT BELOW THIS LINE ///
