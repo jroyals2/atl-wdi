f72aed9897ba648e85f47f8a60a587919e8cad3f
jroyals2 <jamesroyals3@gmail.com>
2017-09-19 18:45:22 -0400

    oops

--- homework/pizza-express/controllers/topping.js
+++ homework/pizza-express/controllers/topping.js
@@ -1,11 +1,11 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
 
 router.get('/:choice', (req, res) => {
     let topping =`${req.params.choice} is a delicious choice`;
     res.render("toppings", {
        topping: topping
-    });
+    }); 
 });
 
 module.exports = router;
