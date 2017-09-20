0f00e6c090144747554f5fc679fc018702b5033e
jroyals2 <jamesroyals3@gmail.com>
2017-09-19 20:27:26 -0400

    Reach goal 1 accomplished

--- homework/pizza-express/server.js
+++ homework/pizza-express/server.js
@@ -5,12 +5,13 @@ const hbs = require('hbs');
 const welcomeController = require('./controllers/welcome.js');
 const orderController = require('./controllers/order.js');
 const toppingController = require('./controllers/topping.js');
-
+const randomController = require('./controllers/random.js');
 app.set("view engine", "hbs");
 
 app.use('/', welcomeController);
 app.use('/order', orderController);
 app.use('/topping', toppingController);
+app.use('*', randomController);
 
 const PORT = 3000;
 app.listen(PORT, () => {
