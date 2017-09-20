cb9e672b693bb7cb5f67808cf9fae36388751c71
jroyals2 <jamesroyals3@gmail.com>
2017-09-19 22:19:58 -0400

    add some fun css

--- homework/pizza-express/server.js
+++ homework/pizza-express/server.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const app = express();
 const hbs = require('hbs');
+//const $ = require('jQuery');
 
 const welcomeController = require('./controllers/welcome.js');
 const orderController = require('./controllers/order.js');
 const toppingController = require('./controllers/topping.js');
 const randomController = require('./controllers/random.js');
-app.set("view engine", "hbs");
 
+
+app.set("view engine", "hbs");
+app.use(express.static(__dirname + '/public'));
 app.use('/', welcomeController);
 app.use('/order', orderController);
 app.use('/topping', toppingController);
