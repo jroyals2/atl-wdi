8d5a6b5c6ca4af553732ebb0626494fbe1a7ffd5
jroyals2 <jamesroyals3@gmail.com>
2017-10-23 17:40:15 -0400

    challenge 6

--- homework/ruby/ruby-ninja.rb
+++ homework/ruby/ruby-ninja.rb
@@ -33,11 +33,24 @@ puts g.to_i + gToNumber
 
 # Challenge 5
 
-puts "What is your name"
-name = gets.chomp
-puts "Input and integer"
-number = gets.chomp
-puts number.to_i * 2
-puts "what do you like to do?"
-favor = gets.chomp
-puts "#{name} likes to #{favor}"
+# puts "What is your name"
+# name = gets.chomp
+# puts "Input and integer"
+# number = gets.chomp
+# puts number.to_i * 2
+# puts "what do you like to do?"
+# favor = gets.chomp
+# puts "#{name} likes to #{favor}"
+
+# Challenge 6
+
+my_first_array = []
+my_first_array = ["What", "Are", "You", "Making me", "Do"]
+my_first_array = my_first_array + [1, 2, 3, 4]
+puts my_first_array.last
+puts my_first_array.first
+my_first_array[1] = "Joe"
+my_first_array[2] = 3
+my_first_array.push(true)
+my_first_array.push(my_first_array[3].class)
+puts my_first_array
